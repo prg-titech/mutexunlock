00067e606010bd746768a29de4b8995b33c3c02a
Qs-F <qsf@de-liker.com>
2021-11-22 03:56:00 +0900

    Add test for playground checks

--- unlockcheck_test.go
+++ unlockcheck_test.go
@@ -1,13 +1,24 @@
 package unlockcheck_test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/Qs-F/unlockcheck"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func Test(t *testing.T) {
+func TestFuncs(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.RunWithSuggestedFixes(t, testdata, unlockcheck.Analyzer, "a")
+}
+
+func TestPlayground(t *testing.T) {
+	testdata := analysistest.TestData()
+	_, err := os.Open(filepath.Join("testdata", "src", "_playground"))
+	if err != nil {
+		return
+	}
+	analysistest.Run(t, testdata, unlockcheck.Analyzer, "_playground")
 }
