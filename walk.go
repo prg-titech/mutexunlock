57729048daf66806cb202a717bbc27108ee56193
Qs-F <qsf@de-liker.com>
2021-12-10 15:43:54 +0900

    Ref walk func to pass next walker to recursive function

--- walk.go
+++ walk.go
@@ -4,32 +4,34 @@ import (
 	"golang.org/x/tools/go/cfg"
 )
 
-type WalkFunc func(block *cfg.Block, ls *LockState)
+// 次に進むか進まないかを決められる関数になっている必要がある
+// 1. callback関数がcontinueするか，stopするかをreturnで決められる
+// 2. callback関数の中でwalkの再起関数を呼び出す
+// traverseの順序を保証するには
+
+type WalkFunc func(block *cfg.Block, ls *LockState, walks func())
 
 func walk(root *cfg.Block, ls *LockState, f WalkFunc, visited VisitedMap) {
-	f(root, ls)
-
-	// SCC
-	if len(root.Succs) == 0 {
+	walks := func() {
+		for _, succ := range root.Succs {
+			e := visited.New(root.Index, succ.Index)
+			ok, err := visited.Visited(e)
+			if err != nil {
+				// TODO: Error handling
+				panic(err)
+			}
+			if ok {
+				continue
+			}
+			if err := visited.Visit(e); err != nil {
+				// TODO: Error handling
+				panic(err)
+			}
+			walk(succ, ls.Copy(), f, visited)
+		}
 	}
 
-	for _, succ := range root.Succs {
-		e := Edge{
-			From: root.Index,
-			To:   succ.Index,
-		}
-		ok, err := visited.Visited(e)
-		if err != nil {
-			// TODO: Error handling
-		}
-		if ok {
-			continue
-		}
-		if err := visited.Done(e); err != nil {
-			// TODO: Error handling
-		}
-		walk(succ, ls.Copy(), f, visited)
-	}
+	f(root, ls, walks)
 }
 
 func Walk(root *cfg.Block, ls *LockState, f WalkFunc, visited VisitedMap) {
