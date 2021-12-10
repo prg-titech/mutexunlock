157cc8437ca6a76dab17749d5bb63211e487b8b3
Qs-F <qsf@de-liker.com>
2021-12-10 15:42:38 +0900

    testdata: Move cyclic -> for

---
+++ testdata/src/a/for.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func _(a bool) {
+	var mu1 sync.Mutex
+
+	for a {
+		mu1.Lock()
+		fmt.Println("hello")
+	} // want "missing unlock"
+}
+
+func _(l []int) int {
+	var mu1 sync.Mutex
+
+	for k, v := range l {
+		mu1.Lock()
+		if v == 0 {
+			return k // want "missing unlock"
+		}
+	} // want "missing unlock"
+
+	return -1 // OK
+}
+
+func _(l []int) int {
+	var mu1 sync.Mutex
+
+	for _, v := range l {
+		mu1.Lock()
+		if v == 0 {
+			fmt.Println("hello")
+			break // want "missing unlock"
+		}
+	} // want "missing unlock"
+
+	return -1 // OK
+}
