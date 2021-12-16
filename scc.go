85a7f051162f3860af5c1967feb7fa7ba381c88d
Qs-F <qsf@de-liker.com>
2021-12-17 07:47:55 +0900

    Fix not used stack issue #16

--- scc.go
+++ scc.go
@@ -1,31 +1,36 @@
 package unlockcheck
 
 import (
-	"golang.org/x/tools/go/cfg"
+	"github.com/Qs-F/unlockcheck/internal/cfg"
 )
 
 var visited = map[Node]struct{}{}
 
 func SCC(root *cfg.Block) (bridges []Edge, attributes map[Node]int, lowlinks map[int][]Node) {
 	visited := make(map[Node]struct{})
+	stack := []Node{}
 	id := 0
 	attributes = make(map[Node]int)
 	lowlinks = make(map[int][]Node)
 	labels := make(map[Node]int)
 	bridges = []Edge{}
 
-	var dfs func(node *cfg.Block)
-	dfs = func(node *cfg.Block) {
+	var dfs func(node *cfg.Block, pre *cfg.Block)
+	dfs = func(node *cfg.Block, pre *cfg.Block) {
 		index := Node(node.Index)
 		visited[index] = struct{}{}
+		stack = append(stack, index)
 		attributes[index] = id
 		labels[index] = id
 		id++
 
 		for _, succNode := range node.Succs {
+			if node == pre {
+				continue
+			}
 			succ := Node(succNode.Index)
 			if _, ok := visited[succ]; !ok {
-				dfs(succNode)
+				dfs(succNode, node)
 				attributes[index] = min(attributes[index], attributes[succ])
 				if labels[index] < attributes[succ] {
 					bridges = append(bridges, Edge{
@@ -33,12 +38,23 @@ func SCC(root *cfg.Block) (bridges []Edge, attributes map[Node]int, lowlinks map
 						To:   int32(succ),
 					})
 				}
-				continue
+			} else {
+				attributes[index] = min(attributes[index], labels[succ])
 			}
-			attributes[index] = min(attributes[index], attributes[succ])
+		}
+		if attributes[index] == labels[index] {
+			for {
+				w := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				delete(visited, w)
+				if w == index {
+					break
+				}
+			}
 		}
 	}
-	dfs(root)
+
+	dfs(root, nil)
 
 	for node, attr := range attributes {
 		lowlinks[attr] = append(lowlinks[attr], node)
