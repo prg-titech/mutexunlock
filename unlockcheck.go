f339ad70dec36c6f0af32c5c5423195fd787c74b
Qs-F <qsf@de-liker.com>
2021-12-20 04:23:53 +0900

    Ref unlockcheck.go
    - Fix to use other refactorings

--- unlockcheck.go
+++ unlockcheck.go
@@ -1,13 +1,11 @@
 package unlockcheck
 
 import (
-	"fmt"
 	"go/ast"
 	"go/token"
 
 	"github.com/Qs-F/unlockcheck/internal/cfg"
 	"github.com/Qs-F/unlockcheck/internal/ctrlflow"
-
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
@@ -34,148 +32,126 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		case *ast.FuncDecl:
 			checkFuncDecl(pass, node)
 			// case *ast.FuncLit:
+			// 	checkFuncLit(pass, node)
 		}
 	})
 
 	return nil, nil
 }
 
-// Inside FuncDecl
-func checkFuncDecl(pass *analysis.Pass, nodeFuncDecl *ast.FuncDecl) {
-	cfgcheck(pass, nodeFuncDecl)
+func checkFuncDecl(pass *analysis.Pass, node *ast.FuncDecl) {
+	cfgcheck(pass, node)
 }
 
-type RetCheck struct {
+// func checkFuncLit(pass *analysis.Pass, node *ast.FuncLit) {
+// 	cfgcheck(pass, node)
+// }
+
+type Check struct {
 	pass       *analysis.Pass
 	blocks     []*cfg.Block
-	bridges    []Edge
+	bridges    Bridges
 	attributes map[Node]int
 	lowlinks   map[int][]Node
-	lastpos    token.Pos
 }
 
-func (rc *RetCheck) Check(block *cfg.Block, ls *LockState, walks func()) {
-	defer walks()
-
+func (rc *Check) Walk(block *cfg.Block, ls *LockState) {
 	for _, node := range block.Nodes {
-		// fmt.Println("|- ", block, node, rc.pass.Fset.Position(node.Pos()))
-		_, op, found, x := NodeToMutexOp(rc.pass, node)
-		if !found {
-			continue
-		}
-		ls.Update(block, x, op)
+		mu := GetMuState(rc.pass, block, node)
+		ls.Push(mu)
 	}
 
+MS:
 	for _, ms := range ls.Map() {
-		if !(ms.Peek().Locked() || ms.Peek().RLocked()) {
+		locked := ms.Peek().Locked()
+		rlocked := ms.Peek().RLocked()
+		if !(locked || rlocked) {
 			continue
 		}
 
 		pos := token.NoPos
 
-	BRIDGE:
-		for _, bridge := range rc.bridges {
+		// the last of for loop
+		for _, succ := range block.Succs { // succはrange.loop または for.loopであることを確認したい
+			if rc.attributes[Node(succ.Index)] == rc.attributes[Node(block.Index)] { // succとblockが同じloopに属している
+				if rc.bridges.IsFrom(Node(succ.Index)) && rc.bridges.IsTo(Node(succ.Index)) { // succはbridgeのfromである (entryとかから)かつsuccはbridgeのtoである (range.done または for.done)
 
-			// the last of for loop
-			for _, succ := range block.Succs { // succはrange.loop または for.loopであることを確認したい
-				// fmt.Println("見てるblock", block, "succ", succ, "bridge", bridge)
-				if rc.attributes[Node(succ.Index)] == rc.attributes[Node(block.Index)] { // succとblockが同じloopに属している
-					if bridge.From == succ.Index { // succはbridgeのfromである (entryとかから)
-						for _, bridge2 := range rc.bridges {
-							if bridge2.To == succ.Index { // かつsuccはbridgeのtoである (range.done または for.done)
-								if len(block.Nodes) > 0 {
-									pos = block.Nodes[len(block.Nodes)-1].End()
-								} else {
-									pos = block.Pos
-								}
-								fmt.Println(rc.pass.Fset.Position(pos).Line)
-								fmt.Println(block)
-								ms.Report(rc.pass, pos, true)
-								ls.Update(block, ms.Peek().node, MutexOpUnlock)
-								break BRIDGE
-							}
-						}
+					if len(block.Nodes) > 0 {
+						pos = block.Nodes[len(block.Nodes)-1].End()
+					} else {
+						pos = block.Pos
 					}
+					// fmt.Println(rc.pass.Fset.Position(pos).Line)
+					// fmt.Println(block)
+					ms.Report(rc.pass, pos, true)
+					ls.Update(block, ms.Peek().node, ms.Peek().Op.Reverse())
+					continue MS
 				}
 			}
+		}
 
-			// break & return
-			if bridge.To == block.Index { // BlockはbridgeのToである
+		// break & return
+		for _, bridge := range rc.bridges {
+			if bridge.To == Node(block.Index) { // BlockはbridgeのToである
 				if len(rc.lowlinks[rc.attributes[Node(bridge.From)]]) > 1 { // BridgeのFromはloopから伸びてる
 					for _, pred := range block.Preds { // predがfor.loopでないことを確認したい (range.doneでないことを確認したい)
-						for _, bridge2 := range rc.bridges {
-							for _, bridge3 := range rc.bridges {
-								if bridge2.From == pred.Index && bridge3.To == pred.Index { // predはどこかのbridgeのfromかつどこかのbridgeのtoではない (range.loopまたは for.loopではない)
-									break BRIDGE
-								}
-							}
+						if rc.bridges.IsFrom(Node(pred.Index)) && rc.bridges.IsTo(Node(pred.Index)) { // predはどこかのbridgeのfromかつどこかのbridgeのtoではない (range.loopまたは for.loopではない)
+							continue MS
 						}
-					} // 上でbreakされなければpredはすべてfor.loopではない
+					}
+					// 上でbreakされなければpredはすべてfor.loopではない
 					if block.Return() != nil {
 						pos = block.Return().Pos()
-						fmt.Println(rc.pass.Fset.Position(pos).Line)
-						fmt.Println(block)
+						// fmt.Println(rc.pass.Fset.Position(pos).Line)
+						// fmt.Println(block)
 						ms.Report(rc.pass, pos, false)
-						ls.Update(block, ms.Peek().node, MutexOpUnlock)
-						break BRIDGE
+						ls.Update(block, ms.Peek().node, ms.Peek().Op.Reverse())
+						continue MS
 					}
 					if len(block.Nodes) > 0 {
 						pos = block.Nodes[len(block.Nodes)-1].End()
 					} else {
 						pos = block.Pos
 					}
-					fmt.Println(rc.pass.Fset.Position(pos).Line)
-					fmt.Println(block)
+					// fmt.Println(rc.pass.Fset.Position(pos).Line)
+					// fmt.Println(block)
 					ms.Report(rc.pass, pos, true)
-					ls.Update(block, ms.Peek().node, MutexOpUnlock)
-					break BRIDGE
+					ls.Update(block, ms.Peek().node, ms.Peek().Op.Reverse())
+					continue MS
 				}
 			}
-
 		}
 
-		if pos != token.NoPos { // 上ですでにreport済み
-			continue
-		} else if block.Return() != nil {
-			fmt.Println(rc.pass.Fset.Position(pos).Line)
-			fmt.Println(block)
+		// }
+
+		if block.Return() != nil {
+			// fmt.Println(rc.pass.Fset.Position(pos).Line)
+			// fmt.Println(block)
 			pos = block.Return().Pos()
 			ms.Report(rc.pass, pos, false)
 		}
 	}
 }
 
-var _ WalkFunc = (&RetCheck{}).Check
+var _ WalkFunc = (&Check{}).Walk
 
 func cfgcheck(pass *analysis.Pass, nodeFuncDecl *ast.FuncDecl) {
 	cfgs, ok := pass.ResultOf[ctrlflow.Analyzer].(*ctrlflow.CFGs)
 	if !ok {
 		return
 	}
-	if cfgs.FuncDecl(nodeFuncDecl) == nil {
+	funcDecl := cfgs.FuncDecl(nodeFuncDecl)
+	if funcDecl == nil {
 		return
 	}
-	if len(cfgs.FuncDecl(nodeFuncDecl).Blocks) < 1 {
+	if len(funcDecl.Blocks) < 1 {
 		return
 	}
 
-	fmt.Println("=============")
-	fmt.Println(pass.Fset.Position(nodeFuncDecl.Pos()), nodeFuncDecl.Name)
+	// fmt.Println("=============")
+	// fmt.Println(pass.Fset.Position(nodeFuncDecl.Pos()), nodeFuncDecl.Name)
 
-	funcDecls := cfgs.FuncDecl(nodeFuncDecl)
-	// fmt.Println(funcDecls)
-
-	// locks := &Locks{
-	// 	pass: pass,
-	// }
-	// Walk(
-	// 	funcDecls.Blocks[0],
-	// 	NewLockState(),
-	// 	locks.Check,
-	// 	NewVisitedEdges(),
-	// )
-
-	bridges, attrs, lowlinks := SCC(funcDecls.Blocks[0])
+	bridges, attrs, lowlinks := NewSCC(funcDecl.Blocks[0])
 
 	// ==================== DEBUG ========================
 	// for _, bridge := range bridges {
@@ -189,18 +165,18 @@ func cfgcheck(pass *analysis.Pass, nodeFuncDecl *ast.FuncDecl) {
 	// fmt.Println("lowlinks: ", lowlinks)
 	// ==================== DEBUG ========================
 
-	// RetCheck
-	retCheck := &RetCheck{
+	check := &Check{
 		pass:       pass,
 		bridges:    bridges,
-		blocks:     funcDecls.Blocks,
+		blocks:     funcDecl.Blocks,
 		attributes: attrs,
 		lowlinks:   lowlinks,
 	}
+
 	Walk(
-		cfgs.FuncDecl(nodeFuncDecl).Blocks[0],
+		funcDecl.Blocks[0],
 		NewLockState(),
-		retCheck.Check,
+		check.Walk,
 		NewVisitedEdges(),
 	)
 }
