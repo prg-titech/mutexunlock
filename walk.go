970f2425cf6817149a31caaf3c20cd65a207cf0f
Qs-F <qsf@de-liker.com>
2021-12-20 04:23:23 +0900

    Ref walk.go
    - Fix not to pass walks func to each WalkFunc

--- walk.go
+++ walk.go
@@ -4,34 +4,26 @@ import (
 	"github.com/Qs-F/unlockcheck/internal/cfg"
 )
 
-// 次に進むか進まないかを決められる関数になっている必要がある
-// 1. callback関数がcontinueするか，stopするかをreturnで決められる
-// 2. callback関数の中でwalkの再起関数を呼び出す
-// traverseの順序を保証するには
-
-type WalkFunc func(block *cfg.Block, ls *LockState, walks func())
+type WalkFunc func(block *cfg.Block, ls *LockState)
 
 func walk(root *cfg.Block, ls *LockState, f WalkFunc, visited VisitedMap) {
-	walks := func() {
-		for _, succ := range root.Succs {
-			e := visited.New(root.Index, succ.Index)
-			ok, err := visited.Visited(e)
-			if err != nil {
-				// TODO: Error handling
-				panic(err)
-			}
-			if ok {
-				continue
-			}
-			if err := visited.Visit(e); err != nil {
-				// TODO: Error handling
-				panic(err)
-			}
-			walk(succ, ls.Copy(), f, visited)
+	f(root, ls)
+	for _, succ := range root.Succs {
+		e := visited.New(Node(root.Index), Node(succ.Index))
+		ok, err := visited.Visited(e)
+		if err != nil {
+			// TODO: Error handling
+			panic(err)
 		}
+		if ok {
+			continue
+		}
+		if err := visited.Visit(e); err != nil {
+			// TODO: Error handling
+			panic(err)
+		}
+		walk(succ, ls.Copy(), f, visited)
 	}
-
-	f(root, ls, walks)
 }
 
 func Walk(root *cfg.Block, ls *LockState, f WalkFunc, visited VisitedMap) {
